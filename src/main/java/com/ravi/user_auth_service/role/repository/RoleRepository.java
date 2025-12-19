539c21daf13ec7d29664c40c48ba36c0efb550fd
Ravi Prajapati <ravi.prajapati@gmail.com>
2025-12-19 15:16:36 +0530

    Add User and Role repositories using Spring Data JPA

--- src/main/java/com/ravi/user_auth_service/role/repository/RoleRepository.java
+++ src/main/java/com/ravi/user_auth_service/role/repository/RoleRepository.java
@@ -1,4 +1,12 @@
 package com.ravi.user_auth_service.role.repository;
 
-public class RoleRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ravi.user_auth_service.role.entity.Role;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+
+    Optional<Role> findByName(String name);
 }
--- src/main/java/com/ravi/user_auth_service/user/repository/UserRepository.java
+++ src/main/java/com/ravi/user_auth_service/user/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.ravi.user_auth_service.user.repository;
 
-public class UserRepository {
+import com.ravi.user_auth_service.user.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository {
+
+
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email); // Used during registration ,Prevents duplicate users
 }
