f73ab888fb4786d2b85b8a6e724489416cb2e5ae
Ravi Prajapati <ravi.prajapati@gmail.com>
2025-12-23 22:00:53 +0530

    Add BCrypt password encryption for registration and login

--- src/main/java/com/ravi/userauthservice/auth/service/AuthServiceImpl.java
+++ src/main/java/com/ravi/userauthservice/auth/service/AuthServiceImpl.java
@@ -2,21 +2,25 @@ package com.ravi.userauthservice.auth.service;
 
 import com.ravi.userauthservice.user.entity.User;
 import com.ravi.userauthservice.user.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 class AuthServiceImpl implements AuthService{
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-   public AuthServiceImpl(UserService userService){
+   public AuthServiceImpl(UserService userService , PasswordEncoder passwordEncoder){
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User authenticate(String email, String password) {
 
        User user= userService.getUserByEmail(email);
-       if(!user.getPassword().equals(password)){
+
+       if(!passwordEncoder.matches(password,user.getPassword())){
            throw new RuntimeException("invalid credential");
        }
         return user;
--- src/main/java/com/ravi/userauthservice/user/service/UserServiceImpl.java
+++ src/main/java/com/ravi/userauthservice/user/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.ravi.userauthservice.role.entity.Role;
 import com.ravi.userauthservice.role.repository.RoleRepository;
 import com.ravi.userauthservice.user.entity.User;
 import com.ravi.userauthservice.user.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -13,10 +14,12 @@ class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository,RoleRepository roleRepository){
+    public UserServiceImpl(UserRepository userRepository,RoleRepository roleRepository , PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -27,6 +30,7 @@ class UserServiceImpl implements UserService{
         Role role = roleRepository.findByName("USER")
                 .orElseThrow(() ->new RuntimeException("Role not found"));
         user.setRoles(Set.of(role));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
